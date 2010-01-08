552391f37cba78ac4b560cc99f181639a7b87584
Sudharshan 'Sup3rkiddo' S <sudharsh@gmail.com>
2010-01-08 23:57:13 +0530

    1.) Add Lexer::update_current_identifier 2.) Handle multiline OBTW and TLDR

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -26,18 +26,31 @@ class Lexer
 {
 private:
     int last_char;
-    std::string identifier; /* The current identifier */    
+    std::string identifier; /* The current identifier */
+    
+    /* Get the current identifier. Also consume characters in the proceess */
+    void update_current_identifier();
 public:
     enum tokens {
         tok_comment = -1,
         tok_invalid = 0
     };
-    
     /* Given a stream of text. Get tokens */
     int get_token();
 };
 
 
+/* Private methods follow */
+void Lexer::update_current_identifier()
+{
+    this->identifier = "";
+    if (isalpha(this->last_char))
+        do { 
+            this->identifier += this->last_char;
+        } while( isalnum((this->last_char = getchar())));
+}
+
+/* Public methods follow */
 int Lexer::get_token()
 {
     this->last_char = ' ';
@@ -47,20 +60,31 @@ int Lexer::get_token()
     while(isspace(this->last_char))
         this->last_char = getchar();
 
-    if (isalpha(this->last_char))
-        do { 
-            this->identifier += this->last_char;
-        } while( isalnum((this->last_char = getchar())));
-    
+    update_current_identifier();        
     cout << "this->identifier" << this->identifier << endl;
 
-    if (this->identifier == "BTW") {
+    /* Single Line comment */
+    if (this->identifier == "BTW") { 
         cout << "Got Comment" << endl;
-        while(this->last_char != EOF && this->last_char != '\n' && this->last_char != '\r');
-        if (this->last_char != EOF) {
+        do
+            this->last_char = getchar();
+        while (this->last_char != EOF && this->last_char != '\n' && this->last_char != '\r');
+                
+        if (this->last_char != EOF) 
             return this->get_token();
-        }
     }
+    /* Multiline comment. ignore till TLDR is reached */
+    else if (this->identifier == "OBTW") {
+        cout << "Got multi-line comment" << endl;
+        while(this->identifier != "TLDR") {
+            this->last_char = getchar();
+            update_current_identifier();
+        }
+        cout << "TLDR reached" << endl;
+        if (this->last_char != EOF)
+            return this->get_token();
+    }
+    
     return Lexer::tok_invalid;
 }
 
